bfee79011286c4b216e99f5cde3ea1267b7141cb
pkkwilliam <pkkwilliam919@gmail.com>
2018-01-22 12:14:42 -0500

    First Commit

---
+++ Run.java
@@ -0,0 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class Run{
+	private static final String TARGET_URL =
+            "https://vision.googleapis.com/v1/images:annotate?";
+	private static final String API_KEY =
+            "key=AIzaSyCRxstIgjv3bTG6bLi1FbcKItcQqu3Xs80";
+    public static void main(String[]args) throws IOException{
+    	URL serverUrl = new URL(TARGET_URL + API_KEY);
+    	URLConnection urlConnection = serverUrl.openConnection();
+    	HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
+    	httpConnection.setRequestMethod("POST");
+    	httpConnection.setRequestProperty("Content-Type", "application/json");
+    	httpConnection.setDoOutput(true);
+    	BufferedWriter httpRequestBodyWriter = new BufferedWriter(new
+        OutputStreamWriter(httpConnection.getOutputStream()));
+		httpRequestBodyWriter.write
+		("{\"requests\":  [{ \"features\":  [ {\"type\": \"WEB_DETECTION\""
+		+"}], \"image\": {\"source\": { \"imageUri\":"
+		+" \"http://www.chiwawadogs.net/wp-content/uploads/2013/06/chipuppy-e1380226702148.jpg\"}}}]}");
+		 httpRequestBodyWriter.close();
+		 String response = httpConnection.getResponseMessage();
+		 if (httpConnection.getInputStream() == null) {
+			   System.out.println("No stream");
+			   return;
+			}
+
+			Scanner httpResponseScanner = new Scanner (httpConnection.getInputStream());
+			String resp = "";
+			while (httpResponseScanner.hasNext()) {
+			   String line = httpResponseScanner.nextLine();
+			   resp += line;
+			   System.out.println(line);  //  alternatively, print the line of response
+			}
+			httpResponseScanner.close();
+    }
+}
